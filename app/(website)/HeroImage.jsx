16f715f53dbbbc6198e8bc158d7bb16154de7ebf
Berzan Özejder <berzanozejder@gmail.com>
2023-11-21 09:07:28 +0200

    more scroll fix

--- app/(website)/HeroImage.jsx
+++ app/(website)/HeroImage.jsx
@@ -5,13 +5,16 @@ import heroImage from "@/public/pages/index/hero2.png";
 import { useEffect, useState } from "react";
 
 export default function HeroImage() {
-	const numbah = 200;
-	const thickness = 0;
 	const [scrollY, setScrollY] = useState(0);
-	const clr = "#000";
+
+	const divisor = 200;
 
 	useEffect(() => {
 		const handleScroll = () => {
+			if (window.scrollY < 0) {
+				setScrollY(0);
+				return;
+			}
 			setScrollY(window.scrollY);
 		};
 
@@ -27,16 +30,16 @@ export default function HeroImage() {
 			<div
 				style={{
 					transform: "translateY(" + scrollY + "px)",
-					opacity: 1 - scrollY / numbah,
+					opacity: 1 - scrollY / divisor,
 				}}
 				className="fixed bottom-0 z-10 hidden max-h-[90vh] min-h-[60vh] md:block">
 				<Image className=" min-h-[65vh] object-cover object-right opacity-90 grayscale" alt="Delagtes having fun" src={heroImage} />
 			</div>
 			<div
 				style={{
-					opacity: 1 - scrollY / numbah,
+					opacity: 1 - scrollY / divisor,
 				}}
-				className={`fixed bottom-0 z-10 max-h-[90vh] min-h-[60vh] md:hidden ${scrollY / numbah < 1 ? "block" : "hidden"}`}>
+				className={`fixed bottom-0 z-10 max-h-[90vh] min-h-[60vh] md:hidden ${scrollY / divisor < 1 ? "block" : "hidden"}`}>
 				<Image className=" min-h-[65vh] object-cover object-right opacity-90 grayscale" alt="Delagtes having fun" src={heroImage} />
 			</div>
 		</>
