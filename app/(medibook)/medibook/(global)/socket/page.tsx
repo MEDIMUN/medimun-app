c7d30d91af851dede9713c535296ab6dcc0cdca0
Berzan Özejder <berzanozejder@gmail.com>
2024-10-04 19:50:05 +0300

    socket test

---
+++ app/(medibook)/medibook/(global)/socket/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+let socket;
+
+const Home = () => {
+	const [input, setInput] = useState("");
+
+	const socketInitializer = async () => {
+		await fetch("http://localhost/api/socket");
+		socket = io();
+
+		socket.on("connect", () => {
+			console.log("connected");
+		});
+
+		socket.on("update-input", (msg) => {
+			setInput(msg);
+		});
+	};
+
+	useEffect(() => {
+		socketInitializer();
+	}, []);
+
+	const onChangeHandler = (e) => {
+		setInput(e.target.value);
+		socket.emit("input-change", e.target.value);
+	};
+
+	return <input placeholder="Type something" value={input} onChange={onChangeHandler} />;
+};
+
+export default Home;
--- hasher.ts
+++ hasher.ts
@@ -1,6 +1,6 @@
 const hashPassword = require("bcryptjs").hash;
 
-const password = "Berzan06!";
+const password = "xnXaEYPcL5z9wnCu";
 
 async function main() {
 	console.log(await hashPassword(password, 12));
