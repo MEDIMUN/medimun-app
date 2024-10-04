126ab35d132d337cc806b070d343372563e6ee2c
Berzan Özejder <berzanozejder@gmail.com>
2024-10-05 01:34:27 +0300

    bigger flower

---
+++ app/api/image/route.tsx
@@ -0,0 +1,8 @@
+import { ImageResponse } from "next/og";
+
+export function GET() {
+	return new ImageResponse(<div tw="flex flex-col rounded-3xl w-full h-full items-center justify-center bg-cover">ELAY</div>, {
+		width: 650,
+		height: 650,
+	});
+}
