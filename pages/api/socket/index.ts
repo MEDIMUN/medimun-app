2e67f4c76409e608495d69bc3635bb24b539dc7c
Berzan Özejder <berzanozejder@gmail.com>
2024-10-04 20:14:54 +0300

    socket patch 2

--- pages/api/socket/index.ts
+++ pages/api/socket/index.ts
@@ -1,7 +1,7 @@
 import { Server } from "socket.io";
 
 const SocketHandler = (req, res) => {
-	if (res.socket.server.io) {
+	if (res?.socket?.server?.io) {
 		console.log("Socket is already running");
 	} else {
 		console.log("Socket is initializing");
