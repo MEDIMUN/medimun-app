9fda7d7ce814397522426985157a31380a197fa7
Berzan Özejder <berzanozejder@gmail.com>
2025-01-28 04:52:11 +0200

    upgr

--- pages/api/socket/index.ts
+++ pages/api/socket/index.ts
@@ -4,7 +4,7 @@ import { NextApiResponse } from "next";
 
 export default function handler(req, res: NextApiResponse) {
 	// Ensure Socket.IO is only initialized once
-	if (!res.socket.server.io) {
+	if (!res?.socket?.server?.io) {
 		console.log("Initializing Socket.IO...");
 		const io = initializeSocket(res.socket.server); // Initialize and attach to the server
 		res.socket.server.io = io; // Attach to Next.js server object for persistence
