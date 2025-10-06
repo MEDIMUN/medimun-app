1210a8c0f28b011130c0807ec8921c54c81d801e
Berzan Özejder <berzanozejder@gmail.com>
2025-10-06 12:21:04 +0200

    0.9.88

--- lib/search-params.ts
+++ lib/search-params.ts
@@ -1,27 +1,28 @@
 "use client";
 
 export function removeSearchParams(params: object, router: any = null) {
-	try {
-		const url = new URL(window.location.href);
-		for (const [key, value] of Object.entries(params)) {
-			url.searchParams.delete(key);
-		}
-		if (router) {
-			router.push(url.toString(), { scroll: false });
-		} else {
-			window.history.replaceState({}, "", url.toString());
-		}
-	} catch (e) {}
+  try {
+    const url = new URL(window.location.href);
+    for (const [key, value] of Object.entries(params)) {
+      url.searchParams.delete(key);
+    }
+    if (router) {
+      window.history.replaceState({}, "", url.toString());
+      router.refresh();
+    } else {
+      window.history.replaceState({}, "", url.toString());
+    }
+  } catch (e) {}
 }
 
 export function updateSearchParams(params: object, router: any = null) {
-	const url = new URL(window.location.href);
-	for (const [key, value] of Object.entries(params)) {
-		url.searchParams.set(key, value);
-	}
-	if (router) {
-		router.push(url.toString(), { scroll: false });
-	} else {
-		window.history.replaceState({}, "", url.toString());
-	}
+  const url = new URL(window.location.href);
+  for (const [key, value] of Object.entries(params)) {
+    url.searchParams.set(key, value);
+  }
+  if (router) {
+    router.push(url.toString(), { scroll: false });
+  } else {
+    window.history.replaceState({}, "", url.toString());
+  }
 }
