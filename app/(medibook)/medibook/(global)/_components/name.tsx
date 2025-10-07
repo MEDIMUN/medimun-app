f7749bac6671bd619c2244ed20de83fe7a969ef3
Berzan Özejder <berzanozejder@gmail.com>
2025-10-07 17:41:44 +0200

    0.9.90

--- app/(medibook)/medibook/(global)/_components/name.tsx
+++ app/(medibook)/medibook/(global)/_components/name.tsx
@@ -3,35 +3,64 @@
 import { Heading } from "@/components/heading";
 import { Text } from "@/components/text";
 import { useSession } from "next-auth/react";
+import DaphneAdvert from "@/public/assets/daphne-advert.png";
+import MediBookWelcome from "@/public/assets/medibook-welcome.webp";
+import Image from "next/image";
 
 export function NameDisplay() {
-	const { data: authSession, status } = useSession();
-	const officialName = authSession?.user?.officialName;
-	const displayName = authSession?.user?.displayName;
-	const preferredName = displayName?.split(" ")[0] || officialName;
-	const userId = authSession?.user?.id;
-	const greeting = new Date().getHours() < 12 ? "Good morning" : new Date().getHours() < 18 ? "Good afternoon" : "Good evening";
+  const { data: authSession, status } = useSession();
+  const officialName = authSession?.user?.officialName;
+  const displayName = authSession?.user?.displayName;
+  const preferredName = displayName?.split(" ")[0] || officialName;
+  const userId = authSession?.user?.id;
+  const greeting =
+    new Date().getHours() < 12 ? "Good morning" : new Date().getHours() < 18 ? "Good afternoon" : "Good evening";
 
-	if (status === "authenticated" && userId)
-		return (
-			<>
-				<Heading as={"p"}>
-					{greeting}, {preferredName}
-				</Heading>
-				<Text as={"p"}>
-					Your User ID is {userId.slice(0, 4)}-{userId.slice(4, 8)}-{userId.slice(8, 12)}
-				</Text>
-			</>
-		);
+  if (status === "authenticated" && userId)
+    return (
+      <>
+        <Heading as={"p"}>
+          {greeting}, {preferredName}
+        </Heading>
+        <Text as={"p"}>
+          Your User ID is {userId.slice(0, 4)}-{userId.slice(4, 8)}-{userId.slice(8, 12)}
+        </Text>
+      </>
+    );
 
-	return (
-		<>
-			<Heading as="p">
-				<span className="animate-pulse bg-gray-200 dark:bg-gray-800 h-8 w-64 rounded-md"></span>
-			</Heading>
-			<Text as={"p"}>
-				<span className="flex mt-1 w-64 h-5 animate-pulse delay-500 bg-gray-100 dark:bg-gray-800 rounded-md"></span>
-			</Text>
-		</>
-	);
+  return (
+    <>
+      <Heading as="p">
+        <span className="h-8 w-64 animate-pulse rounded-md bg-gray-200 dark:bg-gray-800"></span>
+      </Heading>
+      <Text as={"p"}>
+        <span className="mt-1 flex h-5 w-64 animate-pulse rounded-md bg-gray-100 delay-500 dark:bg-gray-800"></span>
+      </Text>
+    </>
+  );
+}
+
+export function WelcomeImage() {
+  const { data: authSession, status } = useSession();
+  const userId = authSession?.user?.id;
+  const isDaphne = userId === "356217598077";
+
+  if (status === "authenticated" && userId)
+    return (
+      <div className="w-full overflow-hidden rounded-xl shadow-md">
+        <Image
+          alt="Welcome to MediBook."
+          quality={100}
+          className="!relative object-cover"
+          src={isDaphne ? DaphneAdvert : MediBookWelcome}
+          fill
+        />
+      </div>
+    );
+
+  return (
+    <div className="w-full overflow-hidden rounded-xl shadow-md">
+      <Image alt="Welcome to MediBook." quality={100} className="!relative object-cover" src={MediBookWelcome} fill />
+    </div>
+  );
 }
--- app/(medibook)/medibook/(global)/page.tsx
+++ app/(medibook)/medibook/(global)/page.tsx
@@ -1,74 +1,68 @@
 import { ActionList } from "@/app/components/actions-list";
-import MediBookWelcome from "@/public/assets/medibook-welcome.webp";
-import Image from "next/image";
-import { NameDisplay } from "./_components/name";
-import prisma from "@/prisma/client";
-import { countries } from "@/data/countries";
+import { NameDisplay, WelcomeImage } from "./_components/name";
 import { TopBar } from "../client-components";
 import { Suspense } from "react";
 import { MainWrapper } from "@/components/main-wrapper";
 
 export default async function Home() {
-	const actions = [
-		{
-			title: "All Sessions",
-			description: "View all sessions",
-			href: `/medibook/sessions`,
-		},
-		{
-			title: "School Director Applications",
-			description: "Applications for the position of School Director",
-			href: `/medibook/sessions/20/apply/school-director`,
-		},
-		{
-			title: "Global Announcements",
-			description: "View all global announcements",
-			href: `/medibook/announcements`,
-		},
-		{
-			title: "Session Announcements",
-			description: "View all the announcements from the latest session",
-			href: `/medibook/sessions/20/announcements`,
-		},
-		{
-			title: "Global Resources",
-			description: "View all global resources",
-			href: `/medibook/resources`,
-		},
-		{
-			title: "Session Resources",
-			description: "View all the resources from the latest session",
-			href: `/medibook/sessions/20/resources`,
-		},
-		{
-			title: "Policies",
-			description: "View conference rules and policies.",
-			href: `/medibook/policies`,
-		},
-		{
-			title: "Account Settings",
-			description: "Change your account settings and add personal information",
-			href: `/medibook/account`,
-		},
-	];
+  const actions = [
+    {
+      title: "All Sessions",
+      description: "View all sessions",
+      href: `/medibook/sessions`,
+    },
+    {
+      title: "School Director Applications",
+      description: "Applications for the position of School Director",
+      href: `/medibook/sessions/20/apply/school-director`,
+    },
+    {
+      title: "Global Announcements",
+      description: "View all global announcements",
+      href: `/medibook/announcements`,
+    },
+    {
+      title: "Session Announcements",
+      description: "View all the announcements from the latest session",
+      href: `/medibook/sessions/20/announcements`,
+    },
+    {
+      title: "Global Resources",
+      description: "View all global resources",
+      href: `/medibook/resources`,
+    },
+    {
+      title: "Session Resources",
+      description: "View all the resources from the latest session",
+      href: `/medibook/sessions/20/resources`,
+    },
+    {
+      title: "Policies",
+      description: "View conference rules and policies.",
+      href: `/medibook/policies`,
+    },
+    {
+      title: "Account Settings",
+      description: "Change your account settings and add personal information",
+      href: `/medibook/account`,
+    },
+  ];
 
-	return (
-		<>
-			<TopBar
-				hideSearchBar
-				hideBreadcrums
-				title={
-					<Suspense fallback="Loading...">
-						<NameDisplay />
-					</Suspense>
-				}
-			/>
-			<MainWrapper>
-				<div className="w-full rounded-xl overflow-hidden shadow-md">
-					<Image alt="Welcome to MediBook." quality={100} className="!relative object-cover" src={MediBookWelcome} fill />
-				</div>
-				<ActionList actions={actions} />
-			</MainWrapper>
-		</>
-	);
+  return (
+    <>
+      <TopBar
+        hideSearchBar
+        hideBreadcrums
+        title={
+          <Suspense fallback="Loading...">
+            <NameDisplay />
+          </Suspense>
+        }
+      />
+      <MainWrapper>
+        <WelcomeImage />
+        <ActionList actions={actions} />
+      </MainWrapper>
+    </>
+  );
 }
