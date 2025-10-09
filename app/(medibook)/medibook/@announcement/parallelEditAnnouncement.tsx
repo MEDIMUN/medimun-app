a9f30889ed98c06f8cb527def76a58528f172a95
Berzan Özejder <berzanozejder@gmail.com>
2025-10-09 21:10:45 +0200

    0.9.91

--- app/(medibook)/medibook/@announcement/parallelEditAnnouncement.tsx
+++ app/(medibook)/medibook/@announcement/parallelEditAnnouncement.tsx
@@ -35,9 +35,8 @@ export function ModalEditAnnouncement({ selectedAnnouncement, authSession }) {
   const [debouncedMarkDown] = useDebouncedValue(markDown, 5000);
 
   function onClose() {
-    if ()
-     removeSearchParams({ "edit-announcement": "" })
-    router.push(pathname);
+    if (pathname) router.push(pathname);
+    removeSearchParams({ "edit-announcement": "" });
   }
 
   async function handleSubmit(formData: FormData) {
